538d289e465bfbb96303ef4c793141ad88e50403
Jacob Reed <jacobreed@Jacobs-MacBook-Pro.local>
2016-09-08 22:26:17 -0500

    rehauled java

--- playfair.java
+++ playfair.java
@@ -1,282 +1,114 @@
-import java.util.*;
+import java.awt.Point;
+import java.util.Scanner;
 
-class PlayfairCipherEncryption
-{
-    private String keyWord = new String();
-    private String key = new String();
-    private char matrix_arr[][] = new char[5][5];
+class PlayfairCipher {
+    private static char[][] charTable;
+    private static Point[] positions;
 
-    // This code sets the key for the playfair cipher
-    public void setkey(String key)
-    {
-        String keyAdjust = new String();
-        boolean flag = false;
-        keyAdjust = keyAdjust + key.charAt(0);
-        for (int i = 1; i < key.length(); i++)
-        {
-            for (int j = 0; j < keyAdjust.length(); j++)
-            {
-                if (key.charAt(i) == keyAdjust.charAt(j))
-                {
-                    flag = true;
-                }
-            }
-            if (flag == false)
-                keyAdjust = keyAdjust + key.charAt(i);
-            flag = false;
-        }
-        keyWord = keyAdjust;
-    }
-
-    public void keyGen()
-    {
-        boolean flag = true;
-        char current;
-        key = keyWord;
-        for (int i = 0; i < 26; i++)
-        {
-            // Gets the ASCII value for the individual characters
-            current = (char) (i + 97);
-            if (current == 'j')
-                continue;
-            for (int j = 0; j < keyWord.length(); j++)
-            {
-                if (current == keyWord.charAt(j))
-                {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag)
-                key = key + current;
-            flag = true;
-        }
-        //System.out.println(key);
-        matrix();
-    }
-
-    // This makes a 5 x 5 matrix from the key text
-    private void matrix()
-    {
-        int counter = 0;
-        for (int i = 0; i < 5; i++)
-        {
-            for (int j = 0; j < 5; j++)
-            {
-                matrix_arr[i][j] = key.charAt(counter);
-                System.out.print(matrix_arr[i][j] + " ");
-                counter++;
-            }
-            System.out.println();
-        }
-    }
-
-    private String format(String originalText)
-    {
-        int i = 0;
-        int len = 0;
-        String text = new String();
-        len = originalText.length();
-        for (int temp = 0; temp < len; temp++)
-        {
-            // If the plaintext has a 'j' it will change that to an 'i' when formatted
-            if (originalText.charAt(temp) == 'j')
-            {
-                text = text + 'i';
-            }
-            else
-                text = text + originalText.charAt(temp);
-        }
-        len = text.length();
-        // Traditionally used when there are dup letters
-        // for (i = 0; i < len; i = i + 2)
-        // {
-        //     // Checks for
-        //     if (text.charAt(i + 1) == text.charAt(i))
-        //     {
-        //         text = text.substring(0, i + 1) + 'x' + text.substring(i + 1);
-        //     }
-        // }
-        return text;
-    }
-
-    private String[] dividPairs(String curString)
-    {
-        String original = format(curString);
-        int size = original.length();
-        // Adds an additional character to the end of the string if not even number of chars
-        if (size % 2 != 0)
-        {
-            size++;
-            original = original + 'x';
-        }
-        String x[] = new String[size / 2];
-        int counter = 0;
-        // Cuts the string into two separate substrings
-        for (int i = 0; i < size / 2; i++)
-        {
-            x[i] = original.substring(counter, counter + 2);
-            counter = counter + 2;
-        }
-        return x;
-    }
-
-    public int[] getDiminsions(char letter)
-    {
-        int[] key = new int[2];
-        if (letter == 'j')
-            letter = 'i';
-        for (int i = 0; i < 5; i++)
-        {
-            for (int j = 0; j < 5; j++)
-            {
-                if (matrix_arr[i][j] == letter)
-                {
-                    key[0] = i;
-                    key[1] = j;
-                    break;
-                }
-            }
-        }
-        return key;
-    }
-
-    /*
-    Encrypts the string using the playfair cipher and appropriate
-    shifting if needed
-    */
-    public String encryptMessage(String Source)
-    {
-        // Divides the plaintext into an array of strings of length two
-        String srcArr[] = dividPairs(Source);
-        String code = new String();
-        char one;
-        char two;
-        //System.out.println(Arrays.toString(srcArr));
-        // Creates a set of two chars. One for the original and one for the resulting.
-        int charOne[] = new int[2];
-        int charTwo[] = new int[2];
-        for (int i = 0; i < srcArr.length; i++)
-        {
-            //System.out.println(Arrays.toString(srcArr));
-            one = srcArr[i].charAt(0);
-            two = srcArr[i].charAt(1);
-
-            // Chars represent the two letters of the pair
-            charOne = getDiminsions(one);
-            charTwo = getDiminsions(two);
-
-            // Checks to see if the rows are the same
-            if (charOne[0] == charTwo[0])
-            {
-                if (charOne[1] < 4)
-                    charOne[1]++; // Moves down a row if they are on the same row
-                else
-                    charOne[1] = 0; // Back to the first row if the end is reached
-                if (charTwo[1] < 4)
-                    charTwo[1]++; // Moves down a row if they are on the same row
-                else
-                    charTwo[1] = 0; // Back to the first row if the end is reached
-            }
-            // Checks to see if the columns are the same
-            else if (charOne[1] == charTwo[1])
-            {
-                if (charOne[0] < 4)
-                    charOne[0]++; // Shift right if not the last column.
-                else
-                    charOne[0] = 0; // Back to the first column if the end is reached
-                if (charTwo[0] < 4)
-                    charTwo[0]++; // Shift right if not the last column.
-                else
-                    charTwo[0] = 0; // Back to the first column if the end is reached
-            }
-            else
-            {
-                int temp = charOne[1];
-                charOne[1] = charTwo[1];
-                charTwo[1] = temp;
-            }
-            // Adds to the cipher text
-            code = code + matrix_arr[charOne[0]][charOne[1]]
-                    + matrix_arr[charTwo[0]][charTwo[1]];
-        }
-        // Returns the cipher text
-        return code;
-    }
-
-    /*
-    This method can be almost the same as the original encryption
-    method but the reverse since all decryption is would be the
-    opposite of encryption.
-    */
-    public String decryptMessage(String Source){
-      String srcArr[] = dividPairs(Source);
-      String code = new String();
-      char one;
-      char two;
-      int charOne[] = new int[2];
-      int charTwo[] = new int[2];
-      for (int i = 0; i < srcArr.length; i++)
-      {
-          one = srcArr[i].charAt(0);
-          two = srcArr[i].charAt(1);
-          charOne = getDiminsions(one);
-          charTwo = getDiminsions(two);
-          if (charOne[0] == charTwo[0])
-          {
-              if (charOne[1] > 0)
-                  charOne[1]--;
-              else
-                  charOne[1] = 4;
-              if (charTwo[1] > 0)
-                  charTwo[1]--;
-              else
-                  charTwo[1] = 4;
-          }
-          else if (charOne[1] == charTwo[1])
-          {
-              if (charOne[0] > 0)
-                  charOne[0]--;
-              else
-                  charOne[0] = 4;
-              if (charTwo[0] > 0)
-                  charTwo[0]--;
-              else
-                  charTwo[0] = 4;
-          }
-          else
-          {
-              int temp = charOne[1];
-              charOne[1] = charTwo[1];
-              charTwo[1] = temp;
-          }
-          code = code + matrix_arr[charOne[0]][charOne[1]]
-                  + matrix_arr[charTwo[0]][charTwo[1]];
-        //  System.out.println(code);
-      }
-      return code;
-    }
-
-    public static void main(String[] args)
-    {
-        PlayfairCipherEncryption x = new PlayfairCipherEncryption();
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a keyWord:");
-        String keyWord = sc.next();
-        x.setkey(keyWord);
-        x.keyGen();
-        System.out.println("Enter word to encrypt: (Make sure length of message is even)");
-        String keyIn = sc.next();
-        if (keyIn.length() % 2 == 0)
-        {
-            System.out.println("Encryption: " + x.encryptMessage(keyIn));
-            System.out.println("Decryption: "
-                    + x.decryptMessage(x.encryptMessage(keyIn)));
+
+        String key = prompt("Enter an encryption key (min length 1): ", sc, 1);
+        String txt = prompt("Enter the message: ", sc, 1);
+        String jti = prompt("Replace J with I? y/n: ", sc, 1);
+
+        boolean changeJtoI = jti.equalsIgnoreCase("y");
+
+        createTable(key, changeJtoI);
+
+        String enc = encode(prepareText(txt, changeJtoI));
+
+        System.out.printf("%nEncoded message: %n%s%n", enc);
+        System.out.printf("%nDecoded message: %n%s%n", decode(enc));
+    }
+
+    private static String prompt(String promptText, Scanner sc, int minLen) {
+        String s;
+        do {
+            System.out.print(promptText);
+            s = sc.nextLine().trim();
+        } while (s.length() < minLen);
+        return s;
+    }
+
+    private static String prepareText(String s, boolean changeJtoI) {
+        s = s.toUpperCase().replaceAll("[^A-Z]", "");
+        return changeJtoI ? s.replace("J", "I") : s.replace("Q", "");
+    }
+
+    private static void createTable(String key, boolean changeJtoI) {
+        charTable = new char[5][5]; // 5 x 5 Matrix creation for the table
+        positions = new Point[26]; // Represnt the 26 different letters of the English Alphabet
+
+        // Prepares the text to be used for the encryption
+        String s = prepareText(key + "ABCDEFGHIJKLMNOPQRSTUVWXYZ", changeJtoI);
+
+        for (int i = 0, k = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (positions[c - 'A'] == null) {
+                charTable[k / 5][k % 5] = c; // Sets the position in the 2D array to be used for the matrix
+                positions[c - 'A'] = new Point(k % 5, k / 5); 
+                k++;
+            }
         }
-        else
-        {
-            System.out.println("Message length should be even");
+    }
+
+    /* Responsible for encoding the string and making sure that the encoded text
+    will be properly formatted when when passed to the codec to finish encoding
+    the plaintext message */
+    private static String encode(String s) {
+        StringBuilder sb = new StringBuilder(s); // Consists of the pre-formatted text made earlier
+
+        for (int i = 0; i < sb.length(); i += 2) {
+            // If the length of the cipher text is odd it will append an 'X' to the end
+            if (i == sb.length() - 1)
+                sb.append(sb.length() % 2 == 1 ? 'X' : "");
+
+            // Inserts an 'X' wherever there is a set of duplicate letter like 'SS'
+            else if (sb.charAt(i) == sb.charAt(i + 1))
+                sb.insert(i + 1, 'X');
         }
-        sc.close();
+        return codec(sb, 1);
+    }
+
+    // Decodes the string using the codec method defined later
+    private static String decode(String s) {
+        return codec(new StringBuilder(s), 4);
+    }
+
+    /* Codec will be used for both the encryption and decryption
+    the direc denotes the way that the shift will move. */
+    private static String codec(StringBuilder text, int direc) {
+        int len = text.length();
+        for (int i = 0; i < len; i += 2) {
+            char a = text.charAt(i);
+            char b = text.charAt(i + 1);
+
+            int row1 = positions[a - 'A'].y;
+            int row2 = positions[b - 'A'].y;
+            int colOne = positions[a - 'A'].x;
+            int colTwo = positions[b - 'A'].x;
+
+            // Shifts the columns to the right if the rows are equivalent
+            if (row1 == row2) {
+                colOne = (colOne + direc) % 5;
+                colTwo = (colTwo + direc) % 5;
+
+            // Shifts the rows down if the columns are equivalent
+            } else if (colOne == colTwo) {
+                row1 = (row1 + direc) % 5;
+                row2 = (row2 + direc) % 5;
+
+            } else {
+                int tmp = colOne;
+                colOne = colTwo;
+                colTwo = tmp;
+            }
+
+            // Gets the character from the table and sets it to the appropriate position
+            text.setCharAt(i, charTable[row1][colOne]); // First
+            text.setCharAt(i + 1, charTable[row2][colTwo]); // Second
+        }
+        return text.toString();
     }
 }
